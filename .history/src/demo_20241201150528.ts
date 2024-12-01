7bc15de5687bf1192875c24786774809c1cb0113
felort1512 <felort15@gmail.com>
2024-12-01 15:11:22 -0600

    First commit, config proyect

---
+++ .history/src/demo_20241201150528.ts
@@ -0,0 +1,2 @@
+type User
+    // "outFile": "./",                                  /* Specify a file that bundles all outputs into one JavaScript file.
---
+++ .history/src/demo_20241201150641.ts
@@ -0,0 +1,2 @@
+type UserId =string | nimber
+    // "outFile": "./",                                  /* Specify a file that bundles all outputs into one JavaScript file.
